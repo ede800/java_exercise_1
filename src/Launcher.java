f0222676d2a709f0e0e6a3f29eb9357649ed3c11
ede800 <edouardzhu@epita.fr>
2022-09-20 16:48:07 +0200

    letsgoooo

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,14 @@
 
 
 public class Launcher { 
+
+  public static int fibonacci(int n) {
+    if (n <= 1) return n;
+    else return fibonacci(n-1) + fibonacci(n-2);
+  }
+  
+
+    
     
   public static void main(String[] args) {
     System.out.println("Bienvenue");
@@ -8,22 +16,33 @@ public class Launcher {
     //une variable une instance de la classe java.util.Scanner dont le constructeur prendra comme paramètre System.in 
     var scanner = new java.util.Scanner(System.in);
 
-    String input = scanner.nextLine();
 
-    System.out.println("Vous avez tapé " + input);
-
-    //if (input.equals("quit")) stop the program
-    if (input.equals("quit")) {
-      System.out.println("Au revoir");
-      System.exit(0);
+    //while input != quit or fibo  print "unknown command" and ask for input again
+    String input = null;
+    while (!"quit".equals(input = scanner.nextLine())) {
+      if(input.equals("fibo")){
+        Integer n = null;
+        System.out.println("Donnez moi un nombre");
+        String number = scanner.nextLine();
+        try{
+          int numberInt = Integer.parseInt(number);
+          System.out.println("Le nombre de fibonacci est " + fibonacci(numberInt));
+        } catch (NumberFormatException e) {
+          System.out.println("Vous n'avez pas entré un nombre");
+        }
+      }
+      else {
+        System.out.println("unknown command");
+      }
     }
-    else {
-      System.out.println("unknown command");
-    }
+
+  }
+}
+    
 
 
-}
-}
+
+
 
 
 
