8783aa30a6d18e8ca9dc8d687ebfbdbc1fc6a0cd
ede800 <edouardzhu@epita.fr>
2022-09-21 17:03:58 +0200

    test

--- src/Launcher.java
+++ src/Launcher.java
@@ -120,37 +120,31 @@ class Quit implements Command {
     return "quit";
   }
   public boolean run(Scanner scanner) {
-    System.out.println("Au revoir");
-    System.exit(0);
-    return false;
+    return true;
   }
 }
 
-
-  
 class Fibo implements Command {
   public String name() {
     return "fibo";
   }
   public boolean run(Scanner scanner) {
+    Integer n = null;
     System.out.println("Donnez moi un nombre");
     String number = scanner.nextLine();
-    try{
+    try {
       int numberInt = Integer.parseInt(number);
       System.out.println("Le nombre de fibonacci est " + fibonacci(numberInt));
     } catch (NumberFormatException e) {
       System.out.println("Vous n'avez pas entré un nombre");
     }
-    return true;
+    return false;
   }
-  public int fibonacci(int n) {
-    if (n <= 1) {
-      return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  public static int fibonacci(int n) {
+    if (n <= 1) return n;
+    else return fibonacci(n-1) + fibonacci(n-2);
   }
 }
-
 
 class Freq implements Command {
   public String name() {
@@ -158,33 +152,28 @@ class Freq implements Command {
   }
   public boolean run(Scanner scanner) {
     System.out.println("Donnez moi le chemin de votre fichier");
-    //lire le contenu du fichier et afficher les 3 mots les plus fréquents
     String path = scanner.nextLine();
     try {
       String content = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(path)));
-      //remove all punctuation by replacing it with a space
       content = content.replaceAll("[^a-zA-Z0-9\\s]", " ");
-      //lowercase all the words
       content = content.toLowerCase();
-      //split the string into an array of words
       String[] words = content.split("\\s+");
-      //is blank
-      if (words.length == 0) {
+      if(words.length == 0){
         System.out.println("Le fichier est vide");
-      } else {
-        //create a hashmap to store the words and their frequency
+      }
+      else{
         java.util.HashMap<String, Integer> wordFrequency = new java.util.HashMap<String, Integer>();
-        for (String word : words) {
-          if (wordFrequency.containsKey(word)) {
+        for(String word : words){
+          if(wordFrequency.containsKey(word)){
             wordFrequency.put(word, wordFrequency.get(word) + 1);
-          } else {
+          }
+          else{
             wordFrequency.put(word, 1);
           }
         }
-        //sort the hashmap by value
         java.util.List<java.util.Map.Entry<String, Integer>> sortedWordFrequency = new java.util.ArrayList<java.util.Map.Entry<String, Integer>>(wordFrequency.entrySet());
-        java.util.Collections.sort(sortedWordFrequency, new java.util.Comparator<java.util.Map.Entry<String, Integer>>() {
-          public int compare(java.util.Map.Entry<String, Integer> entry1, java.util.Map.Entry<String, Integer> entry2) {
+        java.util.Collections.sort(sortedWordFrequency, new java.util.Comparator<java.util.Map.Entry<String, Integer>>(){
+          public int compare(java.util.Map.Entry<String, Integer> entry1, java.util.Map.Entry<String, Integer> entry2){
             return entry2.getValue().compareTo(entry1.getValue());
           }
         });
@@ -197,9 +186,12 @@ class Freq implements Command {
     } catch (java.io.IOException e) {
       System.out.println("Le fichier n'existe pas");
     }
-    return true;
+    return false;
   }
 }
+
+
+
 
 
 
