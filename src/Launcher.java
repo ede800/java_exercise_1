d1262bdaf7573e51dae768a693abe35809e2f436
ede800 <edouardzhu@epita.fr>
2022-09-27 11:29:05 +0200

    pret

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,33 +1,35 @@
 import java.util.*;
 
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Bonjour et bienvenue sur ce code !");
 
-public class Launcher { 
-  public static void main(String[] args) {
-  System.out.println("Bienvenue");
-  var scanner = new java.util.Scanner(System.in);
-  // une variable de type List<Command> et contenant une instance de chaque implémentation de Command
-  java.util.List<Command> commands = new java.util.ArrayList<Command>();
-  commands.add(new Freq());
-  commands.add(new Fibo());
-  commands.add(new Quit());
-  commands.add(new Predict());
-  String input = null;
-  while (!"quit".equals(input = scanner.nextLine())) {
-    boolean found = false;
-    for (Command command : commands) {
-      if (command.name().equals(input)) {
-        found = true;
-        command.run(scanner);
-        break;
-      }
+        Scanner scanner = new Scanner(System.in);
+
+        List<Command> commands = new ArrayList<>();
+        commands.add(new Quit());
+        commands.add(new Fibo());
+        commands.add(new Freq());
+        commands.add(new Predict());
+
+        String res;
+        boolean quit;
+        while (true){
+            res = scanner.nextLine().toLowerCase();
+            quit = false;
+            for (Command command : commands){
+                if (command.name().equalsIgnoreCase(res)){
+                    if (command.run(scanner)){
+                        return;
+                    }
+                    quit = true;
+                }
+            }
+            if (!quit){
+                System.out.println("Unknown command");
+            }
+        }
     }
-    if (!found) {
-      System.out.println("unknown command");
-    }
-  }
-}
-    
-    
 }
     
 
